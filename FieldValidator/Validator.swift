923647137470012734cefb46cab11af05ad87014
iosPremAgravat <prem.ios.bt@gmail.com>
2024-09-18 14:29:40 +0530

    Add Framework

---
+++ FieldValidator/Validator.swift
@@ -0,0 +1,16 @@
+//
+//  Validator.swift
+//  FieldValidation
+//
+//  Created by iMac on 16/09/24.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func validEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
